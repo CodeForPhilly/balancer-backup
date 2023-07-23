80729502f0c6416f5bc9beef736996ff1bc881c3
Paul Baisley <paulbaisley@gmail.com>
2023-07-23 14:28:36 -0400

    fix key warning drop down menu

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -5,13 +5,14 @@ interface DropDownMenuProps {
 }
 
 export const DropDownMenu = ({ links }: DropDownMenuProps) => {
+  console.log("links", links);
   return (
     <div className="font-inter absolute flex flex-row bg-white mt-0 py-2 left-0 right-0 w-full h-72 top-full shadow-lg rounded px-4 sm:px-6 md:px-8 lg:px-8 xl:px-50 2xl:px-56">
       <div className="w-28 mr-9 "></div>
       <div className="">
         <ul className=" mt-8 mb-8">
-          {links.map(({ url, title }) => (
-            <MenuItem title={title} url={url} />
+          {links.map(({ url, title }, i) => (
+            <MenuItem key={`${title}-${i}`} title={title} url={url} />
           ))}
         </ul>
       </div>
--- src/pages/DrugSummary/DrugSummaryForm.tsx
+++ src/pages/DrugSummary/DrugSummaryForm.tsx
@@ -8,13 +8,15 @@ import HourglassSpinner from "../../components/HourglassSpinner/HourglassSpinner
 
 interface FormValues {
   webpage_url: string;
+  pdf: any;
 }
 
 const DrugSummaryForm = () => {
   const [summary, setSummary] = useState("");
+  const [pdf, setPdf] = useState("");
 
   const { error, isLoading, mutate } = useMutation(
-    async (values: { webpage_url: string }) => {
+    async (values: FormValues) => {
       // TODO change this to actual endpoint url when ready
       const res = await axios.post(
         "http://localhost:3001/wpextraction",
@@ -24,12 +26,30 @@ const DrugSummaryForm = () => {
     }
   );
 
-  const { handleChange, handleSubmit, values, resetForm } =
+  // const handleFileUpload = (even: any) => {
+  //   const reader = new FileReader();
+  //   const file = event?.target?.files?.[0];
+  //   reader.onloadend = () => {
+  //     this.setState({
+  //       file: reader.result,
+  //     });
+  //   };
+  //   reader.readAsDataURL(file);
+  // };
+
+  const { handleChange, handleSubmit, resetForm, setFieldValue, values } =
     useFormik<FormValues>({
       initialValues: {
         webpage_url: "",
+        pdf: "",
       },
       onSubmit: (values) => {
+        console.log({
+          pdf: values.pdf,
+          fileName: values.pdf.name,
+          type: values.pdf.type,
+          size: `${values.pdf.size} bytes`,
+        });
         mutate(values, {
           onSuccess: (response) => {
             const message =
@@ -56,7 +76,7 @@ const DrugSummaryForm = () => {
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
-              htmlFor="search"
+              htmlFor="webpage_url"
               className="block text-gray-700 text-sm font-bold mb-2">
               Enter a URL
             </label>
@@ -69,13 +89,42 @@ const DrugSummaryForm = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
+          <p className="font-bold mb-4 text-blue-600">OR</p>
+          <div className="mb-4">
+            <label
+              htmlFor="pdf"
+              className="block text-gray-700 text-sm font-bold mb-2">
+              Upload a PDF
+            </label>
+            <input
+              id="pdf"
+              name="pdf"
+              type="file"
+              onChange={(e) => {
+                const file = e.target?.files?.[0];
+                if (file !== undefined) {
+                  const fileReader = new FileReader();
+                  fileReader.onloadend = () => {
+                    const base64String = fileReader.result as string;
+                    console.log("base64String", base64String);
+                    // Use the base64String as needed
+                    setFieldValue("pdf", base64String);
+                  };
+                  fileReader.readAsDataURL(file);
+                }
+              }}
+              value={values.pdf}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
           <div className="mb-6"></div>
 
           <div className="flex items-center justify-end">
             <button
               className="black_btn disabled:bg-gray-300 disabled:text-gray-600 disabled:border-gray-300"
               type="submit"
-              disabled={!values.webpage_url || isLoading}>
+              // disabled={!values.webpage_url || isLoading}
+            >
               Submit
             </button>
           </div>
