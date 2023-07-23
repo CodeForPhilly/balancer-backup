f1b5a616dc4e530b0c9ed3e783f3b214542eb0ef
Paul Baisley <paulbaisley@gmail.com>
2023-07-23 16:27:36 -0400

    working on pdf upload

--- src/pages/DrugSummary/DrugSummaryForm.tsx
+++ src/pages/DrugSummary/DrugSummaryForm.tsx
@@ -7,8 +7,8 @@ import { useMutation } from "react-query";
 import HourglassSpinner from "../../components/HourglassSpinner/HourglassSpinner";
 
 interface FormValues {
-  webpage_url: string;
-  pdf?: any;
+  url: string;
+  pdf_file: any;
 }
 
 const DrugSummaryForm = () => {
@@ -20,12 +20,19 @@ const DrugSummaryForm = () => {
       // TODO change this to actual endpoint url when ready
       console.log("values", values);
 
-      const form = new FormData();
-      form.append("webpage_url", values.webpage_url);
+      const formData = new FormData();
+      // formData.append("url", values.url);
+      formData.append("pdf", values.pdf_file);
       const res = await axios.post(
         "http://localhost:3001/text_extraction/",
-        values
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
       );
+      console.log('res', res)
       return res;
     }
   );
@@ -44,8 +51,8 @@ const DrugSummaryForm = () => {
   const { handleChange, handleSubmit, resetForm, setFieldValue, values } =
     useFormik<FormValues>({
       initialValues: {
-        webpage_url: "",
-        // pdf: "",
+        url: "",
+        pdf_file: "",
       },
       onSubmit: (values) => {
         // console.log({
@@ -80,16 +87,17 @@ const DrugSummaryForm = () => {
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
-              htmlFor="webpage_url"
+              htmlFor="url"
               className="block text-gray-700 text-sm font-bold mb-2">
               Enter a URL
             </label>
             <input
-              id="webpage_url"
-              name="webpage_url"
+              id="url"
+              name="url"
               type="text"
               onChange={handleChange}
-              value={values.webpage_url}
+              disabled={!!values.pdf_file}
+              value={values.url}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
           </div>
@@ -100,26 +108,33 @@ const DrugSummaryForm = () => {
               className="block text-gray-700 text-sm font-bold mb-2">
               Upload a PDF
             </label>
-            {/* <input
+            <input
               id="pdf"
-              name="pdf"
+              name="pdf_file"
               type="file"
-              onChange={(e) => {
-                const file = e.target?.files?.[0];
-                if (file !== undefined) {
-                  const fileReader = new FileReader();
-                  fileReader.onloadend = () => {
-                    const base64String = fileReader.result as string;
-                    console.log("base64String", base64String);
-                    // Use the base64String as needed
-                    setFieldValue("pdf", base64String);
-                  };
-                  fileReader.readAsDataURL(file);
-                }
-              }}
-              value={values.pdf}
+              disabled={!!values.url}
+              onChange={handleChange}
+              // onChange={(e) => {
+              //   const file = e.target?.files?.[0];
+              //   if (file !== undefined) {
+              //     const fileReader = new FileReader();
+              //     fileReader.onloadend = () => {
+              //       const pdf = fileReader.result as string;
+              //       console.log("base64String", pdf);
+              //       // Use the base64String as needed
+              //       setPdf(pdf);
+              //       // setFieldValue("pdf", pdf);
+              //     };
+              //     fileReader.readAsDataURL(file);
+              //   }
+              // }}
+              // onChange={(e) => {
+              //   console.log('file', e?.currentTarget?.files?.[0])
+              //   setFieldValue("file", e?.currentTarget?.files?.[0]);
+              // }}
+              value={values.pdf_file}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            /> */}
+            />
           </div>
           <div className="mb-6"></div>
 
@@ -127,7 +142,7 @@ const DrugSummaryForm = () => {
             <button
               className="black_btn disabled:bg-gray-300 disabled:text-gray-600 disabled:border-gray-300"
               type="submit"
-              // disabled={!values.webpage_url || isLoading}
+              // disabled={!values.url || isLoading}
             >
               Submit
             </button>
@@ -141,17 +156,17 @@ const DrugSummaryForm = () => {
         {(error as boolean) && (
           <p className="text-center">Please enter a valid URL.</p>
         )}
-        {summary && (
-          <>
-            <h2
-              style={{ fontSize: "2rem" }}
-              className="text-center font-bold my-6">
-              URL Summary
-            </h2>
-            <p>{summary}</p>
-          </>
-        )}
       </section>
+      {summary && (
+        <>
+          <h2
+            style={{ fontSize: "2rem" }}
+            className="text-center font-bold my-6">
+            URL Summary
+          </h2>
+          <p>{summary}</p>
+        </>
+      )}
     </>
   );
 };
